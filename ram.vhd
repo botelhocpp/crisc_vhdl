9588134011a622ea5ee7515fa29f41db8b747db4
Pedro Botelho <pedrobotelho15@alu.ufc.br>
2024-03-30 20:18:24 -0300

    Added memory elements.

---
+++ ram.vhd
@@ -0,0 +1,61 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+ENTITY ram IS
+    PORT (
+        clk : IN STD_LOGIC;
+        we : IN STD_LOGIC;
+        addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        din : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        dout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
+END ram;
+
+ARCHITECTURE hardware OF ram IS
+    TYPE ram_array_t IS ARRAY (0 TO 255) OF STD_LOGIC_VECTOR (7 DOWNTO 0);
+    SIGNAL ram_contents : ram_array_t := (
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00", x"00", x"00", x"00", x"00"
+    );
+BEGIN    
+    PROCESS(clk)
+    BEGIN
+        IF(rising_edge(clk)) THEN
+            IF(we = '1') THEN
+                ram_contents(to_integer(unsigned(addr))) <= din;
+            END IF;
+            dout <= ram_contents(to_integer(unsigned(addr)));
+        END IF;
+    END PROCESS;
+END hardware;
